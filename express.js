8833960a95557ad194a782d04b8694d2e59f41e2
adfar-136 <adfarrasheed136@gmail.com>
2026-04-28 18:05:10 +0530

    28 april

---
+++ express.js
@@ -0,0 +1,36 @@
+
+const express = require("express");
+const app = express();
+
+app.use(express.json())
+
+function myValidation(req,res,next){
+  const {name,age}= req.body;
+  if(!name || !age){
+    return res.status(400).json({
+        error:"Name and age are both required"
+    })
+  } 
+  if(typeof age !== 'number' || age <1){
+    return res.status(400).json({error:"Age must be a positive number"})
+  }
+  next()
+}
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Home Page</h1>")
+})
+
+app.get("/students",(req,res)=>{
+    res.send("<h1>Students Page</h1>")
+})
+
+app.post("/students", myValidation,(req,res)=>{
+    const data = req.body;
+    console.log(data)
+    res.send(data)
+})
+
+app.listen(4000,()=>{
+    console.log("Server is running port 4000")
+})
